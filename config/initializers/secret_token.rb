b9b3c39a1af987c123ec4b1b5abcebdaa1706a5b
kisin <silione@gmail.com>
2012-09-24 22:22:31 +0200

    init sugar admin project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sugaradmin::Application.config.secret_token = 'bc8dad0a1056a8cf197aa5a0717bf8cb74528508849b27d9754cc0efcf162a401672dde291c20a009c142680566029ae81cdef3798524fc99e99223f4b378c71'
