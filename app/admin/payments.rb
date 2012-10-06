aced27f92cd30e89650acc7cf0d0d0a6fd1c180a
kisin <silione@gmail.com>
2012-10-06 20:19:25 +0200

    added index columns

--- app/admin/payments.rb
+++ app/admin/payments.rb
@@ -1,4 +1,16 @@
 ActiveAdmin.register Payment do
 	menu false
 	config.sort_order = "created_at_desc"
+
+
+	index do
+		column :id do |payment|
+			link_to payment.id, admin_payment_path(payment)
+		end
+		column :user_id
+		column :status
+		column :transaction_id
+		column :params
+		column :created_at
+	end
 end
